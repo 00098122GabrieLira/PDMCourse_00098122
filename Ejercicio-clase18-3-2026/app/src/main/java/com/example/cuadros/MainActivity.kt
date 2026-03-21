a7401c31bc43eb4e888aaeccf0451cb6df1a9dff
Gabriel Alexander Lira Andrade <00098122@uca.edu.sv>
2026-03-21 16:30:13 -0600

    Feat: Subir ejecicio de los cuadros de colores

---
+++ Ejercicio-clase18-3-2026/app/src/main/java/com/example/cuadros/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.cuadros
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cuadros.ui.theme.CuadrosTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      CuadrosTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          CuadrosApp(
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun CuadrosApp(modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier.fillMaxSize()
+  ) {
+    // Ejemplo 1 -> Cuadro celeste texto al centro
+    Box(
+      modifier = Modifier
+        .fillMaxWidth()
+        .weight(5f)
+        .background(Color.Cyan),
+      contentAlignment = Alignment.Center
+    ) {
+      Text(
+        text = "Ejemplo 1",
+        fontSize = 14.sp,
+        color = Color.Black
+      )
+    }
+    
+    Row(
+      modifier = Modifier
+        .fillMaxWidth()
+    ){
+      // Ejemplo 2 -> cuadro rojo texto al centro
+      Box(
+        modifier = Modifier
+          .weight(1f)
+          .size(300.dp)
+          .background(Color.Red),
+        contentAlignment = Alignment.Center
+      ) {
+        Text(
+          text = "Ejemplo 2",
+          fontSize = 14.sp,
+          color = Color.Black
+        )
+      }
+      
+      // Ejemplo 3 -> cuadro verde texto en esquina superior izquierda
+      Box(
+        modifier = Modifier
+          .weight(1f)
+          .size(300.dp)
+          .background(Color.Green),
+        contentAlignment = Alignment.TopStart
+      ) {
+        Text(
+          text = "Ejemplo 3",
+          fontSize = 14.sp,
+          color = Color.Black
+        )
+      }
+    }
+    
+    // Ejemplo 4 -> Cuadro rosado texto abajo en el centro
+    Box(
+      modifier = Modifier
+        .fillMaxWidth()
+        .weight(5f)
+        .background(Color.Magenta),
+      contentAlignment = Alignment.BottomCenter
+    ) {
+      Text(
+        text = "Ejemplo 4",
+        fontSize = 14.sp,
+        color = Color.Black
+      )
+    }
+  }
+}
