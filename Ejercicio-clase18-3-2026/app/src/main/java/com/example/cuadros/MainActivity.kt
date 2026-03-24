692d7c35e7ecfeb7fb8457bcbd8ff8cc274b2f32
Gabriel Alexander Lira Andrade <00098122@uca.edu.sv>
2026-03-23 18:41:10 -0600

    Fix: Corregir tamaño de cuadros 2 y 3

--- Ejercicio-clase18-3-2026/app/src/main/java/com/example/cuadros/MainActivity.kt
+++ Ejercicio-clase18-3-2026/app/src/main/java/com/example/cuadros/MainActivity.kt
@@ -8,17 +8,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cuadros.ui.theme.CuadrosTheme
 
@@ -47,7 +46,7 @@ fun CuadrosApp(modifier: Modifier = Modifier) {
     Box(
       modifier = Modifier
         .fillMaxWidth()
-        .weight(5f)
+        .weight(1f)
         .background(Color.Cyan),
       contentAlignment = Alignment.Center
     ) {
@@ -60,13 +59,13 @@ fun CuadrosApp(modifier: Modifier = Modifier) {
     
     Row(
       modifier = Modifier
-        .fillMaxWidth()
+        .fillMaxWidth().weight(1f)
     ){
       // Ejemplo 2 -> cuadro rojo texto al centro
       Box(
         modifier = Modifier
           .weight(1f)
-          .size(300.dp)
+          .fillMaxHeight()
           .background(Color.Red),
         contentAlignment = Alignment.Center
       ) {
@@ -81,7 +80,7 @@ fun CuadrosApp(modifier: Modifier = Modifier) {
       Box(
         modifier = Modifier
           .weight(1f)
-          .size(300.dp)
+          .fillMaxHeight()
           .background(Color.Green),
         contentAlignment = Alignment.TopStart
       ) {
@@ -97,7 +96,7 @@ fun CuadrosApp(modifier: Modifier = Modifier) {
     Box(
       modifier = Modifier
         .fillMaxWidth()
-        .weight(5f)
+        .weight(1f)
         .background(Color.Magenta),
       contentAlignment = Alignment.BottomCenter
     ) {
